fd268b77da97aaa5703b3c0fa690166f51c255cd
sreevaatsavatv <tvs.vaatsava@gmail.com>
2025-09-11 15:41:38 +0530

    profilepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,24 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import LoginPage from './Pages/LoginPage/LoginPage';
-import './index.css';
-import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
-import { HeaderMegaMenu } from './Components/Navbar/HeaderMegaMenu';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import LoginPage from "./Pages/LoginPage/LoginPage";
+import "./index.css";
+import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
+import { HeaderMegaMenu } from "./Components/Navbar/HeaderMegaMenu";
+import ProfilePage from "./Pages/ProfilePage";
 
 function App() {
   return (
     <Router>
-        <HeaderMegaMenu/>
-        <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/login' element={<LoginPage/>} />
-            <Route element={<PrivateRoute/>}>
-
-            </Route>
-        </Routes>
+      <HeaderMegaMenu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route element={<PrivateRoute />}></Route>
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/Pages/ProfilePage.jsx
@@ -0,0 +1,91 @@
+import React, { use, useEffect, useState } from "react";
+
+import Service from "../utils/http";
+import { Center, Container, Text } from "@mantine/core";
+import { Avatar } from "@mantine/core";
+const obj = new Service();
+
+export default function Profile() {
+  const [user, setUser] = useState({});
+
+  const getProfileData = async () => {
+    try {
+      let data = await obj.get("user/me");
+      setUser(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getProfileData();
+  }, []);
+  const [hovered, setHovered] = useState(false);
+  return (
+    <div>
+      {
+        <Center style={{ height: "60vh", width: "100%" }}>
+          <Container
+            size="responsive"
+            style={{
+              backgroundColor: hovered ? "#ffdeeb" : "white",
+              borderRadius: "20px",
+              padding: "20px",
+              transition: "background-color 0.3s ease",
+            }}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+          >
+            <Center height="20vh" mt={50} mb={20}>
+              <Avatar
+                src="https://lh3.googleusercontent.com/a/ACg8ocJNs-Vgoc1mf-DjBC6VDPpG9EA0kezVBUcCfx-L-Ml9oRGQk24R=s96-c"
+                alt="it's me"
+                size={150}
+              />
+            </Center>
+            <Center>
+              <Text
+                size="xl"
+                fw={900}
+                variant="gradient"
+                gradient={{
+                  from: "rgba(0, 0, 0, 1)",
+                  to: "rgba(224, 110, 110, 1)",
+                  deg: 90,
+                }}
+              >
+                {user?.name}
+              </Text>
+            </Center>
+
+            <Center>
+              <Text
+                size="xl"
+                fw={900}
+                variant="gradient"
+                gradient={{
+                  from: "rgba(0, 0, 0, 1)",
+                  to: "rgba(224, 110, 110, 1)",
+                  deg: 90,
+                }}
+              >
+                Email: {user?.email}
+              </Text>
+
+              <br />
+            </Center>
+            <Center>
+              <Text fw={700}>Id: {user?._id}</Text>
+            </Center>
+
+            <Center>
+              <Text fw={700}>Created At: {user?.createdAt}</Text>
+            </Center>
+            <br />
+            {/* </Center> */}
+          </Container>
+        </Center>
+      }
+    </div>
+  );
+}
