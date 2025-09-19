5590a0824623be5fa2f6f8e210f299f5c57b72b5
sreevaatsavatv <tvs.vaatsava@gmail.com>
2025-09-19 18:55:15 +0530

    nav change

--- frontend/src/Components/Navbar/HeaderMegaMenu.jsx
+++ frontend/src/Components/Navbar/HeaderMegaMenu.jsx
@@ -18,7 +18,7 @@ import classes from "./HeaderMegaMenu.module.css";
 const navLinks = [
   { to: "/", label: "Home" },
   { to: "/profile", label: "Profile" },
-  { to: "/url/shortner", label: "Url_Shortner" },
+  { to: "/url/shortener", label: "Url_Shortner" },
   { to: "/url/history", label: "Url_History" },
 ];
 
